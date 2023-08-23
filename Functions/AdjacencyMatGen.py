6b1353b10213cd794cdb31e535db74a1f689b355
eachingateb <eachingateb@unal.edu.co>
2023-08-22 20:31:22 -0500

    Adjacency matrix is now a list, ocupies less space in memory, and allow working with more data

--- Functions/AdjacencyMatGen.py
+++ Functions/AdjacencyMatGen.py
@@ -1,17 +1,23 @@
 import numpy as np
-from mz_Diference import *
-from RT_Diference import *
 import gc
 #Replacing the clustring strategy with the minimum distance
-def AdjacencyMatGen(SignalsMat,max_mz_Tol=5e-3,max_RT_Tol=0.3):
-    DifRT=RT_Diference(SignalsMat)
-    Difmz=mz_Diference(SignalsMat)
-    NumberofCandidates=len(DifRT)
-    AdjacencyMatrix=np.zeros([NumberofCandidates,NumberofCandidates])
-    for candidatePos in np.arange(NumberofCandidates):
-        NeighborsLoc=np.where((DifRT[candidatePos,:]<max_RT_Tol)&(Difmz[candidatePos,:]<max_mz_Tol))[0]
-        AdjacencyMatrix[candidatePos,NeighborsLoc]=1
-    del DifRT
-    del Difmz
+def AdjacencyMatGen(Spectrum,mz_Tol=5e-5,RT_Tol=5):
+    AdjacencyMatrix=[]
+    for signal in Spectrum:
+        mz=signal[0]
+        RT=signal[2]
+        mz_max=mz+mz_Tol
+        mz_min=mz-mz_Tol
+        RT_max=RT+RT_Tol
+        RT_min=RT-RT_Tol
+        NeighborsLoc=np.where((Spectrum[:,0]>mz_min)&(Spectrum[:,0]<mz_max)&(Spectrum[:,2]<RT_max)&(Spectrum[:,2]>RT_min))[0]
+        AdjacencyMatrix.append(NeighborsLoc)
+    del mz
+    del RT
+    del mz_max
+    del mz_min
+    del RT_max
+    del RT_min
+    del NeighborsLoc
     gc.collect()
     return AdjacencyMatrix
